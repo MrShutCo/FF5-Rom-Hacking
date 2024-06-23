6b67a4f31959818b67dae010d8e9f69a6ce3d580
tylerpersonal <tylersafaric99@gmail.com>
2024-06-23 10:44:32 -0400

    Full refactor, now works with some armor, needs more testing

--- bettershop.asm
+++ bettershop.asm
@@ -1,105 +1,139 @@
+; High Level Code Overview
+; 1. Verify that we are in the shop menu
+; 2. Validate its the right type of shop
+; 3. Determine the item type
+; 4. Use it to initialize the correct starting memory address
+; 5. Get the attack/def stat for the item and store it in memory
+; 6. loop through all characters
+;   Get the item id at HeroStatPointer
+;   Get atk/def stat for this characters item
+;   Compare, and set visual to proper one
+;   Keep going until done
+
 hirom
 
-NIDISP     = $2100     ; inital settings for screen
-OBJSEL      = $2101     ; object size $ object data area designation
-OAMADDL     = $2102     ; address for accessing OAM
-OAMADDH     = $2103
-OAMDATA     = $2104     ; data for OAM write
-VMAINC      = $2115     ; VRAM address increment value designation
-VMADDL      = $2116     ; address for VRAM read and write
-VMADDH      = $2117
-VMDATAL     = $2118     ; data for VRAM write
-VMDATAH     = $2119     ; data for VRAM write
-CGADD       = $2121     ; address for CGRAM read and write
-CGDATA      = $2122     ; data for CGRAM write
-TM          = $212c     ; main screen designation
-NMITIMEN    = $4200     ; enable flaog for v-blank
-RDNMI       = $4210     ; read the NMI flag status
-
-;FF5 C2 bank sets DBR to $7E and D to $0000
 bank $7E
 dpbase $0000
 optimize dp always
 optimize address mirrors
 
-StatPointer = $60
-OAMPointer = $62
-YValue = $63
+incsrc "macros.asm"
 
+; ====== Table Starting Pointers ======
 ItemDataStart = $D10000
 ShopDataStart = $D12D40
 
-; Already existing memory
+; ====== Game RAM Addresses ======
 ShopCursorIdx = $155
-ShopID = $135
-CurrAttack = $544
-NewAttack = $1F
+ShopIdx = $135
 
-incsrc "macros.asm"
+; ====== Our RAM Addresses
+HeroStatPointer = $90 ; 2 byte
+ShopAtkDef = $92    ; 1 byte
+ShopItemFirstByte = $93
+TmpWorking = $94    ; Two byte
 
-;org $C2EF59         ; LDA $240,Y ; SEC
-;JSL CheckBetter     ; SBC #$000C
+
+; ====== Starting RAM Item locations ======
+;LeftHandItem = #$513
+;HelmItem = #$50E
+;BodyItem = #$50F
+;RelicItem = #$510 ; ??
+
+; Assumptions:
+;   * Always in 8-bit mode unless we need to switch to 16 bit
+;   * When function starts we are axy16,
 
 org $C2EF1F          ; STZ $F1
-JSL CheckBetter      ; STZ $7E
+JSL BetterShop      ; STZ $7E
 
 org $CFDC00
-CheckBetter: 
-; All of this is done once
+BetterShop:
     PHA
     PHX
     PHY
     PHP
-    %axy8() ; 8bit mode
+    %axy8()
+
+; Validate against RAM that we're in the right menu screen
+IsShopMenu:
     LDA $134            ; Quit early if we're not in a shop
-    CMP #$02            ; This feels like a hack until i know where to hook in
-    BNE end
+    CMP #$02            
+    BEQ IsShopTypeEquipment
+    JMP return
+
+; Validate that the shop type is weapons or equipment
+IsShopTypeEquipment:
     JSL GetShopType
     AND #$0F
     CMP #$01
-    BNE end
-    CLC
-    TXA
-    ADC ShopCursorIdx    ; 9*shopId + cursorIdx
-    TAX
-    LDA $D12D40, X      ; Index into shop data
+    BEQ +
+    CMP #$02
+    BEQ +
+    JMP return
++
 
-    ;STA $2A              ; 12 * itemID + 7 for attack
-    ;LDA $12A             ; Item id
-    %axy16()              ; 16-bit
-    JSR Mult12
-    ADC #$0007
-    TAX
-    LDA #$544           ; Setup attack pointer
-    STA $0160 
-    %a8()        
-    LDA $D10000, X
-    %axy8()       
-    STA NewAttack       ; Store the new attack power
+; Store item atk/def aka byte 7 of item data
+    JSL GetShopItemId
+    LDY #$07
+    JSL GetItemByte
+    STA ShopAtkDef
+
+
+; Store item type
+    JSL GetShopItemId
     LDY #$00
-    LDA #$47
-    STA YValue
+    JSL GetItemByte
+    
+; Switch statement to set initial RAM location for character stats
+    TAY
+    %a16()
+    CPY #$01
+    BEQ relicDefense
+    CPY #$02
+    BEQ armorDefense
+    CPY #$04
+    BEQ helmDefense
+
+weaponAttack:
+    LDA #$513
+    BRA storeStat
+
+helmDefense:
+    LDA #$50E
+    BRA storeStat
+
+armorDefense:
+    LDA #$50F
+    BRA storeStat
+
+relicDefense:
+    LDA #$50F
+    BRA storeStat
+
+storeStat:
+    STA HeroStatPointer ; a16() here
+    LDA #$0000
     LDX #$00
-
-
-; Need to check against each party member
-; X = OAM offset
+    %axy8()
 
 loop:
-    ;LDA $240            ; Make sure we can equip TODO: read this better
-    ;CMP #$E2            
-    ;BEQ CantEquip
-
-    LDA (StatPointer)         ; Get current atk in left hand
-    ;AND #$00FF
-    CMP NewAttack      ; Compare against current
-    BCC CurrBetter
-    ;BEQ CurrEqual
-
-    BCC CurrBetter
+; Get current characters stat and compare
+    LDA (HeroStatPointer)
+    LDY #$07
+    JSL GetItemByte
+    CMP ShopAtkDef
     BEQ CurrEqual
+    BCS WorseOption
 
-CurrWorse:              ; Draw character crouched
+BetterOption:             ; Draw character with arms up
+    LDA $242,X
+    INC
+    INC
+    STA $242,X
+    BRA end
+
+WorseOption:              ; Draw character crouched
     LDA $242,X
     CLC
     ADC #$04
@@ -113,19 +147,13 @@ CurrWorse:              ; Draw character crouched
 CurrEqual:
     BRA end
 
-CurrBetter:             ; Draw character with arms up
-    LDA $242,X
-    INC
-    INC
-    STA $242,X
-    BRA end
-
 end:
     %a16()
-    LDA StatPointer
+    LDA HeroStatPointer
     CLC
     ADC #$0050
-    STA StatPointer
+    STA HeroStatPointer
+    AND #$00FF              ; Clear high byte so next mult works for item
     %a8()
     TXA
     CLC
@@ -133,6 +161,7 @@ end:
     TAX
     CPX #$20
     BNE loop
+
 return:
     %a16()            ; X,Y are 8bit mode
     PLP
@@ -144,47 +173,79 @@ return:
     STZ $7E
 RTL
 
-CantEquip:
-    LDA $00
-    STA $20C,X
-    STA $20D,X
-    STA $20E,X
-    STA $20F,X
-    BRA end
+org $CFDE00
 
+; GetItemByte gets the Yth byte of data from an 
+; Keeps current state of X
+GetItemByte:
+    PHX
+    JSR Mult12 ; A in 16bit
+    %xy16()
+    STY TmpWorking
+    CLC
+    ADC TmpWorking
+    TAX
+    LDA ItemDataStart, X
+    AND #$00FF
+    %axy8()
+    PLX
+    RTL
+
+; GetAtkDef gets the atk/def of an item id stored in A
+; Doesnt modify 8/16bit
+GetAtkDef:
+    JSR Mult12 ; A in 16bit
+    CLC
+    ADC #$0007
+    %xy16()
+    TAX       
+    %a8()   ; Get only 1 byte of data
+    LDA ItemDataStart, X
+    %axy8()
+    RTL
+
+
+; GetShopType sets A to first byte of shop data
+; Doesn't modify 8/16bit
+GetShopType:
+    LDA ShopIdx
+    JSR Mult9
+    TAX
+    %a8()
+    LDA ShopDataStart, X
+    RTL
+
+; GetShopItemId sets A to the item id based on the current cursors index
+; Doesn't modify 8/16bit
+GetShopItemId:
+    LDA ShopIdx
+    JSR Mult9
+    %a8()
+    CLC
+    ADC ShopCursorIdx   ; TODO: does this add in 8bit mode screw up anything?
+    TAX
+    LDA $D12D40, X
+    RTL
+
+; Multiplies an 8-bit value in A by 9 and keeps it in A
+; Doesn't modify 8/16bit
+Mult9:
+    STA $2A
+    %a16()
+    ASL
+    ASL
+    ASL
+    %a8()
+    ADC $2A
+    RTS
+
+; Multiplies an 8-bit value in A by 12 and keeps it in A
+; Sets A to 16-bit
 Mult12:
+    %a16()
     ASL
     ASL         ; x4
     STA $002A     
     ASL         ; x8
     ADC $002A     ; x(8+4)
     RTS 
-
-org $0FDE00
-GetShopType:
-    LDA $135            ; ShopId
-    %a16()
-    JSR Mult9
-    TAX
-    %a8()
-    LDA ShopDataStart,X        ; Get the type of shop
-    RTL
-
-; Sets A to itemId
-GetItemId:
-    LDA ShopID
-    JSR Mult9
-    CLC
-    ADC ShopCursorIdx
-    TAX
-    LDA $D12D40, X
-    RTL
-
-Mult9:
-    STA $002A
-    ASL
-    ASL
-    ASL
-    ADC $002A
-    RTS
-
